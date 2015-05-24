6797c3527d5a3917cb06c3e87425d69d45ab6e80
toleksyn <taras.oleksyn@bazaarvoice.com>
2015-05-24 15:24:10 +0000

    update

--- src/main/java/automation/pagefactory/VerifyGooglePages.java
+++ src/main/java/automation/pagefactory/VerifyGooglePages.java
@@ -2,7 +2,7 @@ package automation.pagefactory;
 
 import automation.pagefactory.pageobjects.HomePage;
 import automation.pagefactory.pageobjects.SearchResultsPage;
-import junit.framework.Assert;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class VerifyGooglePages extends TestRunner{
@@ -15,7 +15,7 @@ public class VerifyGooglePages extends TestRunner{
         final SearchResultsPage searchResultsPage = homePage.searchFor("happy");
         homePage = searchResultsPage.clickGoogleLogo();
 
-        Assert.assertTrue("Input field not displayed",
-                homePage.getInputFiedWebElement().isDisplayed());
+        Assert.assertTrue(homePage.getInputFiedWebElement().isDisplayed(),
+                "Input field not displayed");
     }
 }
